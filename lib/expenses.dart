53513d6739dd6b3a57e88439d9da3355c8b1a88c
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-05 09:59:34 +0600

    Start Expense tracker Building

---
+++ lib/expenses.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text('THE Chart'),
+          Text('Expenses List'),
+          ],
+      ),
+    );
+  }
+}
