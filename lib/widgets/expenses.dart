3d36a1de912fef15c0b9f442670518c0107a5747
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-05 17:31:51 +0600

    create list start

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Courses',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 12.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('THE Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
