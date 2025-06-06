13b4366a9f9c5c0e1df7df9361d83fd46eaa728a
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-06 17:52:16 +0600

    creating add button working

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
@@ -27,9 +28,20 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Fluter ExpenseTracker'),
+        actions: [IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           Text('THE Chart'),
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: const [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+        ],
+      ),
+    );
+  }
+}
