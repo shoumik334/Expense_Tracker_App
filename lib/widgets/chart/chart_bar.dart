09655d314b2df79c83eac5e633072d90968d56a7
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-11 23:19:57 +0600

    add comment in chart bar

--- lib/widgets/chart/chart_bar.dart
+++ lib/widgets/chart/chart_bar.dart
@@ -7,12 +7,14 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Check if the current theme is in dark mode
     final isDarkMode =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4),
         child: FractionallySizedBox(
+          // This widget allows us to size the child (the bar) based on a fraction of the available space
           heightFactor: fill,
           child: DecoratedBox(
             decoration: BoxDecoration(
@@ -20,11 +22,12 @@ class ChartBar extends StatelessWidget {
               borderRadius: const BorderRadius.vertical(
                 top: Radius.circular(8),
               ),
-               color:
+              color:
                   isDarkMode
                       ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.primary.withValues(alpha: 0.3),
-             
+                      : Theme.of(
+                        context,
+                      ).colorScheme.primary.withValues(alpha: 0.3),
             ),
           ),
         ),
